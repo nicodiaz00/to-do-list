d83561b34180360712e2a256f0e57a78bc598129
Nico Diaz <ngdiaz00@gmail.com>
2025-07-07 21:00:41 -0300

    Se crea archivo sv

---
+++ backend/server.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+
+
+app.use(express.json());
+
+app.get('/tasks',(req, res) =>{
+
+})
+
+app.post('/tasks',(req, res)=> {
+
+})
+
+app.put('/tasks/:id',(req, res)=>{
+
+})
+
+app.delete('/tasks/:id',(req, res)=>{
+
+})
